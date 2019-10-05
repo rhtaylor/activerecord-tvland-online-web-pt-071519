c4dafdfdb41f504b91fb683eed6b959e76437bc6
Randy Taylor <randyisbusy@gmail.com>
2019-10-05 22:40:58 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+  def full_name
+    x = self.first_name
+    y = self.last_name
+    "#{x} #{y}"
+  end
+  def list_roles
+    y = self.characters
+    x = self.shows
+  "#{y[0].name} - #{x[0].name}"
+  end
+
+
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,17 @@
 class Character < ActiveRecord::Base
-  
+   belongs_to :actor
+   belongs_to :show
+   def say_that_thing_you_say
+      self.catchphrase
+      "#{self.name} always says: #{self.catchphrase}"
+    end
+    def build_network
+      w = self.show
+      x = self.network
+      binding.pry
+      "#{x}"
+    end
+    def build_show(arg)
+      show.name = arg[:name]
+    end
 end
